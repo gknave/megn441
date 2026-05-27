71de695295898379abdc3759364bd93bf7278766
Gary Nave <gary.k.nave@gmail.com>
2026-05-27 16:18:05 -0600

    Removed unnecessary cd from startDocker.sh

--- startDocker.sh
+++ startDocker.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-cd /mnt/c/Users/garynave/Documents/docker_ws
 docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /mnt/c/Users/garynave/Documents/docker_ws:/mnt/local \
   -w /mnt/local/ros2_ws \
