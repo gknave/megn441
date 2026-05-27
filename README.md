7dbaf3c749fa222016df45d1847ed492cef44fc4
Gary Nave <gary.k.nave@gmail.com>
2026-05-27 15:44:24 -0600

    Added Simulation package, bringup package, and modified gitignore. Also added brief install description to README.md

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # MEGN 441: Intro to Robotics
+
+This is the home repository for the labs of MEGN 441: Intro to Robotics at Colorado School of Mines.
+
+## Quick start
+
+See ROS2 Install for further guidance. With Docker installed, navigate to directory with `Dockerfile` and run
+
+```` bash
+docker build -t ros2-dev .
+````
+
+Once you have built the docker image using the Dockerfile, modify `startDocker.sh` to capture the path to your local me441 directory with `ros2_ws`. You may need to make the shell script executable with:
+
+```` bash
+chmod +777 startDocker.sh
+````
+
+Once `startDocker.sh` is executable, you can run it with:
+
+```` bash
+./startDocker.sh
+````
---
+++ ros2_ws/src/bringup/launch/bringup.launch.py
@@ -0,0 +1,32 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from launch import LaunchDescription, LaunchService
+from launch.actions import IncludeLaunchDescription, OpaqueFunction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def launch_setup(context):
+    startup_check_node = Node(
+        package='bringup',
+        executable='startup_check',
+        output='screen',
+    )
+
+    return [
+            startup_check_node,
+            ]
+
+def generate_launch_description():
+    return LaunchDescription([
+        OpaqueFunction(function = launch_setup)
+    ])
+
+if __name__ == '__main__':
+    # Create a LaunchDescription object
+    ld = generate_launch_description()
+
+    ls = LaunchService()
+    ls.include_launch_description(ld)
+    ls.run()
